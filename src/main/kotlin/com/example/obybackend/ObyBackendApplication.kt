d7f5ae901b22391a4ac694a86b3b397fae7e164a
junichi.hattori <junhat6@gmail.com>
2025-10-13 11:27:03 +0900

    first commit

---
+++ src/main/kotlin/com/example/obybackend/ObyBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.example.obybackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ObyBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<ObyBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/obybackend/ObyBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.obybackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ObyBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
