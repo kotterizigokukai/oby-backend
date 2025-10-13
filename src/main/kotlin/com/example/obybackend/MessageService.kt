164e596facd9685d281ef1b6546b39ae02f57f8b
junichi.hattori <junhat6@gmail.com>
2025-10-13 18:21:03 +0900

    github actions 修正

--- src/main/kotlin/com/example/obybackend/MessageService.kt
+++ src/main/kotlin/com/example/obybackend/MessageService.kt
@@ -1,25 +1,28 @@
 package com.example.obybackend
 
-import org.springframework.stereotype.Service
 import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.core.query
-import java.util.*
+import org.springframework.stereotype.Service
+import java.util.UUID
 
 @Service
 class MessageService(private val db: JdbcTemplate) {
-    fun findMessages(): List<Message> = db.query("select * from messages") { response, _ ->
-        Message(response.getString("id"), response.getString("text"))
-    }
+    fun findMessages(): List<Message> =
+        db.query("select * from messages") { response, _ ->
+            Message(response.getString("id"), response.getString("text"))
+        }
 
-    fun findMessageById(id: String): Message? = db.query("select * from messages where id = ?", id) { response, _ ->
-        Message(response.getString("id"), response.getString("text"))
-    }.singleOrNull()
+    fun findMessageById(id: String): Message? =
+        db.query("select * from messages where id = ?", id) { response, _ ->
+            Message(response.getString("id"), response.getString("text"))
+        }.singleOrNull()
 
     fun save(message: Message): Message {
         val id = message.id ?: UUID.randomUUID().toString() // Generate new id if it is null
         db.update(
             "insert into messages values ( ?, ? )",
-            id, message.text
+            id,
+            message.text,
         )
         return message.copy(id = id) // Return a copy of the message with the new id
     }
